bef6df354297e635650c14ca3557283f00cd0526
Brett Haines <BHaines418@gmail.com>
2022-02-05 18:59:17 -0500

    Convert action timer to function

--- src/items/Safe.gd
+++ src/items/Safe.gd
@@ -13,9 +13,7 @@ func _open_box(player):
 	# First check if box is locked
 	if locked:
 		if player.keyring.find(key_id) != -1:
-			open_timer.start(unlock_time)
-			emit_signal("action_started", "Unlocking...", unlock_time)
-			await open_timer.timeout
+			await _wait_for_timer(unlock_time, "Unlocking...")
 			locked = false
 			player.remove_key_id(key_id)
 		else:
@@ -24,13 +22,16 @@ func _open_box(player):
 	# Only open if not locked and not already opening
 	if not locked and open_timer.is_stopped():
 		if open_time > 0:
-			open_timer.start(open_time)
-			emit_signal("action_started", "Opening...", open_time)
-			await open_timer.timeout
+			await _wait_for_timer(unlock_time, "Opening...")
 		
 		print("Opening safe!")
 		set_interacted(true)
 		$AnimationPlayer.play("DoorAction002")
+
+func _wait_for_timer(time, label):
+	open_timer.start(time)
+	emit_signal("action_started", label, time)
+	await open_timer.timeout
 
 func _close_box():
 	set_interacted(false)
