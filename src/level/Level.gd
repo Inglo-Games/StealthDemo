b4af906027b0720ce832a1c5365cf9744a04fd1e
Brett Haines <BHaines418@gmail.com>
2022-05-17 22:53:55 -0400

    Add noisemaker placing function to level class

--- src/level/Level.gd
+++ src/level/Level.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 var dialogue_window = preload("res://src/dialog/DialogueWindow.tscn")
+var noisemaker = preload("res://src/items/Noisemaker.tscn")
 
 
 func _ready():
@@ -14,6 +15,9 @@ func _ready():
 	for guard in $Guards.get_children():
 		if guard is Guard:
 			$Player.emit_noise.connect(guard.on_hear_noise)
+	
+	# DEBUG: Add a noisemaker to scene
+	place_noisemaker(Vector3(0, 0, -30), 15)
 
 
 func _process(delta):
@@ -21,6 +25,18 @@ func _process(delta):
 		var pause_menu = load("res://src/menus/PauseMenu.tscn").instantiate()
 		add_child(pause_menu)
 		get_tree().paused = true
+
+
+# Create a new NoiseMaker object and place it in the specified position
+func place_noisemaker(pos:Vector3, magnitude:int):
+	var item = noisemaker.instantiate()
+	add_child(item)
+	item.position = pos
+	
+	# Connect new noisemaker to all guards
+	for guard in $Guards.get_children():
+		if guard is Guard:
+			item.emit_noise.connect(guard.on_hear_noise)
 
 
 func resume_from_dialogue():
