e62968dddf670e6af5e8f7ef4602a5d8b3c55c8d
Brett Haines <BHaines418@gmail.com>
2022-05-17 23:21:28 -0400

    Add initial items menu

---
+++ src/menus/ItemMenu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_lockpick_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_noise_button_pressed():
+	pass # Replace with function body.
