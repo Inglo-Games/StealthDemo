6da33488bf3df6f811725b4c682e201872e54bac
Brett Haines <BHaines418@gmail.com>
2022-05-31 22:45:50 -0400

    Add navigation support to guard

--- src/guard/Guard.gd
+++ src/guard/Guard.gd
@@ -16,11 +16,12 @@ const CHASE_THRESHOLD : int = 30         # Distance at which guard stops chase
 const COOLDOWN_TIME : int = 20           # Seconds until Guard loses "alert" state
 
 @export_node_path(Path3D) var patrol_path
+@onready var nav_agent : NavigationAgent3D = $NavigationAgent3D
 
 var patrol_points
 var patrol_index := 0
 var state := GUARD_STATE.PATROL
-var target
+var target_player : Player
 
 
 func _ready():
@@ -31,7 +32,7 @@ func _ready():
 	# Set initial target point
 	if patrol_path:
 		patrol_points = get_node(patrol_path).curve.get_baked_points()
-		target = patrol_points[patrol_index]
+		nav_agent.set_target_location(patrol_points[0])
 	else:
 		print("Warning: Guard has no path to patrol!")
 
@@ -43,24 +44,42 @@ func _physics_process(_delta):
 			# Set speed based on state
 			var current_speed = BASE_SPEED if state == GUARD_STATE.PATROL else CHASE_SPEED
 			# Move toward next point if not already there
-			if position.distance_squared_to(target) >= 1:
-				_move_toward_target(target, current_speed)
+			if position.distance_squared_to(nav_agent.get_target_location()) >= 1:
+				_move_toward_target(current_speed)
 			# If close to target and patrol_points isn't just one point, 
 			# change to next point in patrol_points
 			elif patrol_points.size() > 1:
 				patrol_index = (patrol_index + 1) % len(patrol_points)
-				target = patrol_points[patrol_index]
+				nav_agent.set_target_location(patrol_points[patrol_index])
+		
 		GUARD_STATE.SEARCHING:
 			# TODO: Implement searching func
 			pass
+		
 		GUARD_STATE.CHASE:
-			_move_toward_target(target.get_transform().origin, CHASE_SPEED)
+			# Recalculate path to player using NavigationAgent
+			nav_agent.set_target_location(target_player.global_transform.origin)
+			_move_toward_target(CHASE_SPEED)
 			# Stop chasing if player is too far away
-			if position.distance_to(target.get_transform().origin) > CHASE_THRESHOLD:
+			if position.distance_to(nav_agent.get_target_location()) > CHASE_THRESHOLD:
 				_enter_state_alert()
-				target = patrol_points[patrol_index]
+				nav_agent.set_target_location(patrol_points[patrol_index])
+		
 		_:
 			print("Guard process error: invalid GUARD_STATE")
+
+
+func _move_toward_target(speed : float):
+	var next_pos : Vector3 = nav_agent.get_next_location()
+	var curr_pos : Vector3 = global_transform.origin
+	var new_vec : Vector3 = (next_pos - curr_pos).normalized() * speed
+	nav_agent.set_velocity(new_vec)
+
+
+# Function moves guard using vector to target point using NavigationServer
+func _on_nav_velocity_computed(safe_velocity : Vector3):
+	velocity = safe_velocity
+	move_and_slide()
 
 
 # Function triggered when any object enters the "SightArea" Area3D
@@ -68,7 +87,7 @@ func _on_object_spotted(body):
 	# If Player is spotted, enter chase state
 	if body is Player and body.state != body.MOVE_STATE.HIDING:
 		state = GUARD_STATE.CHASE
-		target = body
+		target_player = body
 		$TempLabel.show_label_temp(3, "!")
 	# Else if interactable object is spotted and it's been interacted with,
 	# become suspicious
@@ -81,20 +100,6 @@ func _on_object_caught(body):
 	if body is Player:
 		print("Caught player!")
 		get_tree().change_scene_to(load("res://src/menus/MainMenu.tscn"))
-
-
-func _move_toward_target(target, speed):
-	# Look toward target
-	look_at(target)
-	
-	# Calculate direction to target, ignoring vertical difference
-	var dir : Vector3 = target - self.get_transform().origin
-	dir.y = 0
-	dir = dir.normalized() * speed
-	
-	velocity.x = dir.x
-	velocity.z = dir.z
-	move_and_slide()
 
 
 func on_hear_noise(noise_origin, noise_magnitude):
