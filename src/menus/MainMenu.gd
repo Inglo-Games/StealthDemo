05c8946bee778c23c81f8d6f3718a2fa1043cd4d
Brett Haines <BHaines418@gmail.com>
2022-06-03 23:14:42 -0400

    Remove original dev level for new one

--- src/menus/MainMenu.gd
+++ src/menus/MainMenu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to(load("res://src/level/Level.tscn"))
+	get_tree().change_scene_to(load("res://src/level/DevLevel.tscn"))
 
 
 func _on_quit_button_pressed():
