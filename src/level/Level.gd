9aa6e44a3af11f2ab2cd798787353a75d9a97154
Brett Haines <BHaines418@gmail.com>
2022-07-27 23:20:12 -0400

    Generalize Level script

--- src/level/Level.gd
+++ src/level/Level.gd
@@ -1,13 +1,24 @@
 extends Node3D
+class_name Level
 
-var dialogue_window = preload("res://src/dialog/DialogueWindow.tscn")
-var noisemaker = preload("res://src/items/Noisemaker.tscn")
+# Preload widely used scenes
+var dialogue_window_scene = preload("res://src/dialog/DialogueWindow.tscn")
+var noisemaker_scene = preload("res://src/items/Noisemaker.tscn")
+var pause_scene = preload("res://src/menus/PauseMenu.tscn")
+
+var dialogue_window = null
 
 
 func _ready():
 	
+	print("Entered Level _ready function")
+	
 	# Lock mouse motion
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+	# Set up dialogue window
+	dialogue_window = dialogue_window_scene.instantiate()
+	add_child(dialogue_window)
 	
 	# Connect player's noise signal to guards
 	for guard in $Guards.get_children():
@@ -20,14 +31,14 @@ func _ready():
 
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_cancel"):
-		var pause_menu = load("res://src/menus/PauseMenu.tscn").instantiate()
+		var pause_menu = pause_scene.instantiate()
 		add_child(pause_menu)
 		get_tree().paused = true
 
 
 # Create a new NoiseMaker object and place it in the specified position
 func place_noisemaker(pos:Vector3, magnitude:int):
-	var item = noisemaker.instantiate()
+	var item = noisemaker_scene.instantiate()
 	item.position = pos
 	item.sound_strength = magnitude
 	add_child(item)
