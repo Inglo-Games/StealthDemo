126a6373de05330518cdc3e6f05df54606a1d508
Brett Haines <BHaines418@gmail.com>
2022-07-31 22:46:25 -0400

    Fix guard in tutorial 2 not resetting player

--- src/level/TutorialRoom02.gd
+++ src/level/TutorialRoom02.gd
@@ -3,6 +3,7 @@ extends Level
 # Keep track of which dialogues have been triggered
 var dialogues_triggered := [ false, false, false ]
 
+@onready var guard = $Guards/Guard
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,12 +11,16 @@ func _ready():
 	# Run parent class _ready func first
 	super._ready()
 	
+	# Guard should reset player instead of game over
+	guard.player_caught.disconnect(_on_player_caught)
+	guard.player_caught.connect(_on_player_reset)
+	
 	# Immediately load the first dialogue
 	_load_dialogue_01()
 	
 	# Load second and third dialogues after being caught
-	$Guards/Guard.player_caught.connect(_load_dialogue_03)
-	$Guards/Guard.player_caught.connect(_load_dialogue_02)
+	guard.player_caught.connect(_load_dialogue_03)
+	guard.player_caught.connect(_load_dialogue_02)
 	
 	# Load final dialogue when player reaches exit
 	$NavigationRegion3D/LevelExitDoor.action_started.connect(_load_dialogue_04)
