2f673d70aa6aaa9add17db267922ae03fcfbafee
Brett Haines <BHaines418@gmail.com>
2022-08-13 23:24:16 -0400

    Add second dialogue to tutorial room 5

--- src/level/TutorialRoom05.gd
+++ src/level/TutorialRoom05.gd
@@ -6,5 +6,13 @@ func _ready():
 	# Run parent class _ready func first
 	super._ready()
 	
+	# Immediately load first dialogue and give player a Noisemaker item
 	$Player.inventory["noisemakers"] += 1
 	dialogue_window.initialize_conversation("res://assets/dialogues/tut05_01.json")
+	
+	# Load final when player reaches exit
+	$NavigationRegion3D/LevelExitDoor.action_started.connect(_load_dialogue_02)
+
+
+func _load_dialogue_02():
+	dialogue_window.initialize_conversation("res://assets/dialogues/tut05_02.json")
