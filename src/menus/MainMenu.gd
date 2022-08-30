67b0c8bb8dc986d9a32bdc32e58734b3ac918a31
Brett Haines <BHaines418@gmail.com>
2022-08-30 16:32:30 -0400

    Remove dev level

--- src/menus/MainMenu.gd
+++ src/menus/MainMenu.gd
@@ -5,10 +5,6 @@ func _on_start_button_pressed():
 	get_tree().change_scene_to(load("res://src/level/TutorialRoom01.tscn"))
 
 
-func _on_dev_button_pressed():
-	get_tree().change_scene_to(load("res://src/level/DevLevel.tscn"))
-
-
 func _on_credits_button_pressed():
 	get_tree().change_scene_to(load("res://src/menus/CreditsMenu.tscn"))
 
