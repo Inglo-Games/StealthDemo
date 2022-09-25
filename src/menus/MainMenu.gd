3cff6f8572ea501f352f83aac95457711504fdfe
Brett Haines <BHaines418@gmail.com>
2022-09-25 00:03:09 -0400

    Set mouse mode in main menu

--- src/menus/MainMenu.gd
+++ src/menus/MainMenu.gd
@@ -1,6 +1,10 @@
 extends Control
 
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+
 func _on_start_button_pressed():
 	get_tree().change_scene_to(load("res://src/level/TutorialRoom01.tscn"))
 
