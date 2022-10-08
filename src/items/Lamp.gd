1d93668d574da1e9acb133c974c265c350c4702b
Brett Haines <BHaines418@gmail.com>
2022-10-08 14:01:24 -0400

    Add lamp sfx

--- src/items/Lamp.gd
+++ src/items/Lamp.gd
@@ -2,6 +2,9 @@ extends Interactable
 class_name Lamp
 
 
+const on_sound = preload("res://assets/sfx/lamp_on.wav")
+const off_sound = preload("res://assets/sfx/lamp_off.wav")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Lamps aren't locked or have complicated mechanisms
@@ -12,6 +15,12 @@ func _ready():
 # Turn the lamp off or on, opposite of current state
 func interact(player):
 	
+	if is_interacted():
+		$AudioStreamPlayer3D.stream = off_sound
+	else:
+		$AudioStreamPlayer3D.stream = on_sound
+	
 	var curr = not is_interacted()
 	$OmniLight3D.visible = curr
+	$AudioStreamPlayer3D.play()
 	set_interacted(curr)
