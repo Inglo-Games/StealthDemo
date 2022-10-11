3c4e5be41cbf85b2fba628d35561cf8df781cea8
Brett Haines <BHaines418@gmail.com>
2022-10-11 16:07:50 -0400

    Add safe opening sound

---
+++ src/items/Safe.gd
@@ -0,0 +1,7 @@
+extends ItemBox
+class_name Safe
+
+func _ready():
+	# Load item-specific sound resources
+	open_sound = load("res://assets/sfx/safe_open.wav")
+	close_sound = load("res://assets/sfx/safe_open.wav")
