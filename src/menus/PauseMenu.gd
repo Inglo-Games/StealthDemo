5d9f5ead985c361230888a72d6e3be07e3704fc1
Brett Haines <BHaines418@gmail.com>
2022-10-25 23:28:47 -0400

    Add volume slider to pause menu

--- src/menus/PauseMenu.gd
+++ src/menus/PauseMenu.gd
@@ -2,9 +2,9 @@ extends Control
 class_name PauseMenu
 
 
-
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	$Panel/VBoxContainer/VolSlider.value = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master"))
 
 
 # Tell the main game to resume and remove this scene
@@ -12,6 +12,11 @@ func _on_resume_button_pressed():
 	get_tree().paused = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	self.queue_free()
+
+
+# Apply audio changes to master bus
+func _on_vol_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
 
 
 # Quit the game
